d08cec0946d2a3bcfc8fbe1145cc34c8ce62b712
NakanishiTetsuhiro <kaoru6strings@gmail.com>
2015-07-24 01:45:20 +0900

    food-box-1のfood-name-boxだけを変更させることに成功。

--- js/my.js
+++ js/my.js
@@ -1,7 +1,8 @@
 $(function() {
   $(".kind-box").change(function() {
-      var kindBoxValue = $('select[name="kindBox"]').val();
-      // alert(kindBoxValue);
+    var kindBoxValue = $('select[name="kindBox"]').val();
+    // alert(kindBoxValue);
+    var tempId = $(this).parents('[id^=food-box-]').attr('id');
 
     $.ajax({
         type: "POST",
@@ -15,7 +16,9 @@ $(function() {
         // alert(data);
         // console.log(data[0]);
         // alert('success!!');
-        $('.food-name-box > option').remove();
+        // var tempId = $(this).closest(li).attr('id');
+        console.log(tempId);
+        $('#' + tempId + ' .food-name-box > option').remove();
         for (var i = 0; i < data.length; i++){
           $('.food-name-box').append($('<option>').html(data[i]["japanese"]).val(data[i]["id"]));
         }
