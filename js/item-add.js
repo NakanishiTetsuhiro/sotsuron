dae15ffe85dc5e5b85ff4c6e61dc2b40dcd04725
NakanishiTetsuhiro <kaoru6strings@gmail.com>
2015-07-27 15:47:37 +0900

    追加したアイテムのAjaxがうまく動いてくれない。。

--- js/item-add.js
+++ js/item-add.js
@@ -1,6 +1,6 @@
-$(document).ready(function(){
+$(function() {
 
-  var idNum = 1;
+  var idNum = 0;
 
   // "品目の追加"ボタンを押した場合の処理
   $('#btn_add').click(function(){
@@ -9,12 +9,50 @@ $(document).ready(function(){
       scriptCharset: 'utf-8',
       url: "item-add.php",
     }).done(function(data){
+      // console.log(data);
+
+      // テンプレートにIDを設定
       idNum++;
-      $('#food-list').append(data);
-      $('#food-box-template').attr('id', "food-box-" + idNum);
+      var foodBoxTemp = data.replace(/food-box-template/, 'food-box-' + idNum);
+
+      $('#food-list').append(foodBoxTemp);
+
+      $("#food-box-" + idNum + " > div > div > div > .kind-box").on({change: function() {
+          // alert("できた！！");
+
+          var kindBoxValue = $('select[name="kindBox"]').val();
+          var tempId = $(this).parents('[id^=food-box-]').attr('id');
+          $.ajax({
+            type: "POST",
+            scriptCharset: 'utf-8',
+            dataType: "json",
+            url: "get-food-name.php",
+            data: {
+              item:kindBoxValue
+            },
+          }).done(function(data){
+            console.log(tempId);
+            $('#' + tempId + ' .food-name-box > option').remove();
+            for (var i = 0; i < data.length; i++){
+              $('.food-name-box').append($('<option>').html(data[i]["japanese"]).val(data[i]["id"]));
+            }
+          }).fail(function(data){
+            alert('error!!!' + data);
+          });
+
+        }
+      });
+      console.log("チェック");
+
+      // $('#food-box-template').attr('id', "food-box-" + idNum);
+
+      // $('#food-box-' + idNum + ' .kindBox').on("change", function(){
+      //   alert("click!");
+      // });
+
+
     }).fail(function(data){
       alert('error!!!');
     });
   });
 });
-
--- js/my.js
+++ js/my.js
@@ -2,7 +2,7 @@ $(function() {
   $(".kind-box").change(function() {
     var kindBoxValue = $('select[name="kindBox"]').val();
     // alert(kindBoxValue);
-    var tempId = $(this).parents('[id^=food-box-]').attr('id');
+    var foodBoxId = $(this).parents('[id^=food-box-]').attr('id');
 
     $.ajax({
         type: "POST",
@@ -16,9 +16,11 @@ $(function() {
         // alert(data);
         // console.log(data[0]);
         // alert('success!!');
-        // var tempId = $(this).closest(li).attr('id');
-        console.log(tempId);
-        $('#' + tempId + ' .food-name-box > option').remove();
+        // var foodBoxId = $(this).closest(li).attr('id');
+
+        console.log(foodBoxId);
+
+        $('#' + foodBoxId + ' .food-name-box > option').remove();
         for (var i = 0; i < data.length; i++){
           $('.food-name-box').append($('<option>').html(data[i]["japanese"]).val(data[i]["id"]));
         }
