0ca18a0e1afb7116de4651efcb4ec6997ac2fd67
NakanishiTetsuhiro <kaoru6strings@gmail.com>
2015-07-29 21:41:39 +0900

    金額を表示するところがうまくいかない

--- result.php
+++ result.php
@@ -27,7 +27,8 @@
   <div class="container">
 
     <section class="inner-box">
-<!--
+
+<!-- 料理の種類を表示させるとこ（旧）
        <h1 class="kind-box">
         <?php
         try {
@@ -42,45 +43,26 @@
         ?>
       </h1>
 -->
-<!--
-      <div class="item">
-        <p class="chinese">
-          <?php
-          try {
-            $sql= "SELECT id, chinese FROM Mlang WHERE id = $idTemp";
-            $stmh = $pdo->prepare($sql);
-            $stmh->execute();
-            $row = $stmh->fetchall(PDO::FETCH_ASSOC);
-          } catch (PDOException $Exception) {
-            print "エラー：" . $Exception->getMessage();
-          }
-          echo $row[0]['chinese'];
-          ?>
-        </p>
-        <p class="food-option">miso soup, rice, OKINAWA noodle set.</p>
-        <p class="price"><?php echo $_POST["price"] ?> YEN</p>
-      </div>
--->
+
 
 <?php
   $i = 0;
   $itemCounter = count($_POST["food-name-box"]);  // 料理の品目数を取得
 
   foreach ($_POST as $key => $value) {
-    // 品目の数だけdivタグを生成
-    if ($i == 0) {
+    if ($i == 0) {                        // 品目の数だけdivタグを生成
       $item = [];
       for ($j=0; $j < $itemCounter; $j++) {
         $item[$j] = "<div class=\"item\">";
       }
     }
 
-    // 選択された言語で料理名を表示
-    if ($key == "lang-select") {
+    if ($key == "lang-select") {           // 選択された言語で料理名を表示
       foreach ($value as $key => $lang) {
+        // var_dump($lang);
         if ($lang == "chinese") {
+          $j = 0;
           foreach ($_POST["food-name-box"] as $key => $id) {
-            $j = 0;
             // var_dump($id);
             try {
               $sql= "SELECT id, chinese FROM Mlang WHERE id = $id";
@@ -90,7 +72,7 @@
             } catch (PDOException $Exception) {
               print "エラー：" . $Exception->getMessage();
             }
-            // var_dump($row[0]['chinese']);
+            // var_dump($row[0]);
             $item[$j] .= "<h2 class=\"chinese\">".$row[0]['chinese']."</h2>";
             $j++;
           }
@@ -98,15 +80,25 @@
       }
     }
 
-    if ($i == $itemCounter) {
+//---------------ここがうまくいかない--------------------------
+    // if ($key == "price") {                                    // 金額を表示
+    //   foreach ($value as $key => $price) {
+    //   $j = 0;
+    //     var_dump($price);
+    //     $item[$j] .= "<p class=\"price\">".$price."YEN</p>";
+    //     // $item[$j] .= "入ってる？？";
+    //     $j++;
+    //   }
+    // }
+//------------------------------------------------------------
+
+    if ($i == $itemCounter) {                           // タグを閉じる処理と出力の処理
       for ($j=0; $j < $itemCounter; $j++) {
         $item[$j] .= "</div>";
         echo "$item[$j]";
       }
     }
     $i++;
-    // echo "i= $i, ";
-    // echo "$key<br>";
   }
 ?>
 
