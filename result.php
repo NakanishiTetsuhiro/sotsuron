74c6af7d9fe217e0b2d814d58e579ce18e144c61
NakanishiTetsuhiro <kaoru6strings@gmail.com>
2015-09-17 19:01:03 +0900

    20150917 last commit

---
+++ result.php
@@ -0,0 +1,144 @@
+  <!-- Database connect settings loading -->
+<?php require_once('connect-db.php'); ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>メニュー表出力結果</title>
+    <link rel="stylesheet" type="text/css" href="css/reset.css">
+    <link rel="stylesheet" href="css/result.css">
+  </head>
+  <body class="content-print">
+    <div class="wrapper">
+      <h1 class="store_name"><?php echo $_POST["store-name"]; ?></h1>
+      <div class="container">
+        <section class="inner-box">
+          <?php
+          $i = 0;
+
+          // 料理の品目数を取得
+          $foodCounter = count($_POST["food-name-box"]);
+          foreach ($_POST as $key => $value) {
+
+          // 品目の数だけdivタグを生成
+          if ($i == 0) {
+          $item = array();
+          for ($j=0; $j < $foodCounter; $j++) {
+          $item[$j] = "<div class=\"item\">";
+            }
+            $k = 0;
+
+            // 画像を表示させる
+            foreach ($_POST["food-name-box"] as $idKey => $id) {
+            // var_dump($id);
+            try {
+              $sql= "SELECT id, img_path FROM Mlang WHERE id = $id";
+              // print $sql."<br>";
+              $stmh = $pdo->prepare($sql);
+              $stmh->execute();
+              $row = $stmh->fetchall(PDO::FETCH_ASSOC);
+            } catch (PDOException $Exception) {
+              print "エラー：" . $Exception->getMessage();
+            }
+            // db_accessor("id, img_path", "Mlang", "id = $id");
+            $item[$k] .= "<img class=\"food-img\" src=". $row[0]['img_path'] .">";
+            $k++;
+            }
+            }
+
+            // 選択された言語で料理名を表示
+            if ($key == "lang-select") {
+            for ($j=0; $j < $foodCounter; $j++) {
+            $item[$j] .= "<div class=\"lang-box\">";
+              }
+              foreach ($value as $langKey => $lang) {
+              // var_dump($lang);
+              // 中国語
+              if ($lang == "chinese") {
+              $j = 0;
+              foreach ($_POST["food-name-box"] as $idKey => $id) {
+              // var_dump($id);
+              try {
+              $sql= "SELECT id, chinese FROM Mlang WHERE id = $id";
+              $stmh = $pdo->prepare($sql);
+              $stmh->execute();
+              $row = $stmh->fetchall(PDO::FETCH_ASSOC);
+              } catch (PDOException $Exception) {
+              print "エラー：" . $Exception->getMessage();
+              }
+              // var_dump($row[0]);
+              $item[$j] .= "<h2 class=\"chinese\">".$row[0]['chinese']."</h2>";
+              $j++;
+              }
+              }
+              // 日本語
+              if ($lang == "japanese") {
+              $j = 0;
+              foreach ($_POST["food-name-box"] as $idKey => $id) {
+              try {
+              $sql= "SELECT id, japanese FROM Mlang WHERE id = $id";
+              $stmh = $pdo->prepare($sql);
+              $stmh->execute();
+              $row = $stmh->fetchall(PDO::FETCH_ASSOC);
+              } catch (PDOException $Exception) {
+              print "エラー：" . $Exception->getMessage();
+              }
+              $item[$j] .= "<p class=\"japanese\">".$row[0]['japanese']."</p>";
+              $j++;
+              }
+              }
+              }
+              for ($j=0; $j < $foodCounter; $j++) {
+            $item[$j] .= "</div>";
+            }
+            }
+
+            // オプションを表示
+            // if ($key == "food-option") {
+            //   $j = 0;
+            //   var_dump($value);
+            //   foreach ($value as $foodOptionKey => $foodOption) {
+            //     // if ($_POST["tax"] == "included") {
+            //     //   $item[$j] .= "<p class=\"food-option\">".$foodOption."</p>";
+            //     // } elseif ($_POST["tax"] == "exclusive") {
+            //     //   $item[$j] .= "<p class=\"food-option\">".$foodOption."</p>";
+            //     // }
+            //     $j++;
+            //   }
+            // }
+
+            // 金額を表示
+            if ($key == "price") {
+            $j = 0;
+            foreach ($value as $priceKey => $price) {
+            // echo "$j<BR>";
+            // var_dump($price);
+            // echo "<BR>";
+            if ($_POST["tax"] == "included") {
+            $item[$j] .= "<p class=\"price\">".$price."YEN</p>";
+            } elseif ($_POST["tax"] == "exclusive") {
+            $item[$j] .= "<p class=\"price\">".$price."YEN +TAX</p>";
+            }
+            $j++;
+            }
+            }
+            $postCounter = count($_POST);
+            $postCounter--;
+
+            // タグを閉じる処理と出力の処理
+            if ($i == $postCounter) {
+            for ($j=0; $j < $foodCounter; $j++) {
+          $item[$j] .= "</div>";
+          echo $item[$j];
+          }
+          }
+          $i++;
+          // echo $i;
+          }
+          ?>
+        </section>
+      </div>
+    </div>
+  </body>
+</html>
--- setting.php
+++ setting.php
@@ -1,96 +1,91 @@
+<!-- Database connect settings loading -->
 <?php require_once('connect-db.php'); ?>
 
 <!DOCTYPE html>
 <html lang="ja">
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-  <title>メニュー表作成画面</title>
-
-  <!-- Bootstrap -->
-  <link href="bootstrap-3.3.4-dist/css/bootstrap.min.css" rel="stylesheet">
-
-  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>メニュー表作成画面</title>
+    <!-- Bootstrap -->
+    <link href="bootstrap-3.3.4-dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-      <![endif]-->
-
-  <link href="css/style.css" rel="stylesheet" type="text/css">
-</head>
-
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+  </head>
   <body id="setting">
     <div class="top">
       <header>
         <div class="container">
           <a href="index.php"><h1>食堂メニュー変換サービス</h1></a>
         </div>
-      </header><!-- /header -->
-    </div><!-- /top -->
+        </header><!-- /header -->
+        </div><!-- /top -->
 
-    <section class="container">
-      <form action="result.php" method="post" accept-charset="utf-8">
-      <div class="item-box">
-        <div class="row">
-          <div class="col-md-4">
-            <div class="form-group">
-              <label for="store-name">店舗名</label>
-              <input type="text" name="store-name" class="form-control" id="store-name" placeholder="店舗名を入力してください">
+        <section class="container">
+            <form action="result.php" method="post" accept-charset="utf-8">
+            <div class="item-box">
+              <div class="row">
+                <div class="col-md-4">
+                  <div class="form-group">
+                    <label for="store-name">店舗名</label>
+                    <input type="text" name="store-name" class="form-control" id="store-name" placeholder="店舗名を入力してください">
+                  </div>
+                </div>
+                <div class="col-md-4">
+                  <label for="languages" class="block-label">表示したい言語</label>
+                  <label class="checkbox-inline">
+                    <input type="checkbox" name="lang-select[]" class="languages" value="chinese"> 中国語
+                  </label>
+                  <label class="checkbox-inline">
+                    <input type="checkbox" name="lang-select[]" class="languages" value="japanese"> 日本語
+                  </label>
+                  <!--
+                   <label class="checkbox-inline">
+                    <input type="checkbox" name="lang-select[]" id="languages" value="romaji" disabled="disabled"> ローマ字
+                  </label>
+                  <label class="checkbox-inline">
+                    <input type="checkbox" id="inlineCheckbox3" value="option3"> 英語
+                  </label>
+                  -->
+                </div>
+                <div class="col-md-4">
+                  <label for="exampleInputEmail1" class="block-label">消費税の表示</label>
+                  <label class="radio-inline">
+                    <input type="radio" name="tax" id="inlineRadio1" value="included">内税
+                  </label>
+                  <label class="radio-inline">
+                    <input type="radio" name="tax" id="inlineRadio2" value="exclusive">外税
+                  </label>
+                </div>
+              </div> <!-- /row -->
+            </div> <!-- /item-box -->
+
+              <ul id="food-list"></ul>
+
+              <div class="row">
+              <button type="button" id="btn_add" class="btn btn-default pull-right">品目を追加する</button>
             </div>
-          </div>
+            <div class="row">
+              <button type="submit" class="btn btn-primary btn-lg pull-right" style="margin-top:20px;">メニュー表を作る</button>
+            </div>
+          <div id="test"></div>
+        </form>
+      </section>
 
-          <div class="col-md-4">
-            <label for="languages" class="block-label">表示したい言語</label>
-            <label class="checkbox-inline">
-              <input type="checkbox" name="lang-select[]" id="languages" value="chinese"> 中国語
-            </label>
-            <label class="checkbox-inline">
-              <input type="checkbox" name="lang-select[]" id="languages" value="japanese"> 日本語
-            </label>
-            <label class="checkbox-inline">
-              <input type="checkbox" name="lang-select[]" id="languages" value="romaji" disabled="disabled"> ローマ字
-            </label>
-<!--
-            <label class="checkbox-inline">
-              <input type="checkbox" id="inlineCheckbox3" value="option3"> 英語
-            </label>
--->
-          </div>
-          <div class="col-md-4">
-            <label for="exampleInputEmail1" class="block-label">消費税の表示</label>
-            <label class="radio-inline">
-              <input type="radio" name="tax" id="inlineRadio1" value="included">内税
-            </label>
-            <label class="radio-inline">
-              <input type="radio" name="tax" id="inlineRadio2" value="exclusive">外税
-            </label>
-          </div>
-        </div> <!-- /row -->
-      </div> <!-- /item-box -->
+    <?php require_once('footer.php'); ?>
 
-      <ul id="food-list"></ul>
-
-      <div class="row">
-        <button type="button" id="btn_add" class="btn btn-default pull-right">品目を追加する</button>
-      </div>
-      <div class="row">
-        <button type="submit" class="btn btn-primary btn-lg pull-right" style="margin-top:20px;">メニュー表を作る</button>
-      </div>
-    <div id="test"></div>
-      </form>
-    </section>
-
-
-<?php require_once('footer.php'); ?>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="js/jquery-1.11.3.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="bootstrap-3.3.4-dist/js/bootstrap.min.js"></script>
-
     <script src="js/food-box-add.js"></script>
   </body>
 </html>
