437c2cfb4b047a12a034cb1260a8b23950f2cffc
NakanishiTetsuhiro <kaoru6strings@gmail.com>
2015-10-10 11:52:00 +0900

    Create food-option

--- result.php
+++ result.php
@@ -31,47 +31,71 @@
             if ($i == 0) {
               $item = array();
               for ($j=0; $j < $foodCounter; $j++) {
-              $item[$j] = "<div class=\"item\">";
-                }
-                $k = 0;
+                $item[$j] = "<div class=\"item\">";
+              }
 
-                // 画像を表示させる
-                foreach ($_POST["food-name-box"] as $idKey => $id) {
-                  $get_image = $db->db_accessor("id, img_path", "Mlang", "id = $id");
-                  $item[$k] .= "<img class=\"food-img\" src=". $get_image[0]['img_path'] .">";
-                  $k++;
+              // 画像を表示させる
+              $k = 0;
+              foreach ($_POST["food-name-box"] as $idKey => $id) {
+                $get_image = $db->db_accessor("id, img_path", "Mlang", "id = $id");
+                $item[$k] .= "<img class=\"food-img\" src=". $get_image[0]['img_path'] .">";
+                $k++;
+              }
+            }
+
+            // 選択された言語で料理名を表示
+            if ($key == "lang-select") {
+              for ($j=0; $j < $foodCounter; $j++) {
+                $item[$j] .= "<div class=\"lang-box\">";
+              }
+              foreach ($value as $langKey => $lang) {
+                // 中国語
+                if ($lang == "chinese") {
+                  $j = 0;
+                  foreach ($_POST["food-name-box"] as $idKey => $id) {
+                    $get_chinese = $db->db_accessor("id, chinese", "Mlang", "id = $id");
+                    $item[$j] .= "<h2 class=\"chinese\">".$get_chinese[0]['chinese']."</h2>";
+                    $j++;
+                  }
+                }
+                // 日本語
+                if ($lang == "japanese") {
+                  $j = 0;
+                  foreach ($_POST["food-name-box"] as $idKey => $id) {
+                    $get_japanese = $db->db_accessor("id, japanese", "Mlang", "id = $id");
+                    $item[$j] .= "<p class=\"japanese\">".$get_japanese[0]['japanese']."</p>";
+                    $j++;
+                  }
                 }
               }
 
-              // 選択された言語で料理名を表示
-              if ($key == "lang-select") {
-                for ($j=0; $j < $foodCounter; $j++) {
-                  $item[$j] .= "<div class=\"lang-box\">";
-                }
-                foreach ($value as $langKey => $lang) {
-                  // 中国語
-                  if ($lang == "chinese") {
-                    $j = 0;
-                    foreach ($_POST["food-name-box"] as $idKey => $id) {
-                      $get_chinese = $db->db_accessor("id, chinese", "Mlang", "id = $id");
-                      $item[$j] .= "<h2 class=\"chinese\">".$get_chinese[0]['chinese']."</h2>";
-                      $j++;
-                    }
-                  }
-                  // 日本語
-                  if ($lang == "japanese") {
-                    $j = 0;
-                    foreach ($_POST["food-name-box"] as $idKey => $id) {
-                      $get_japanese = $db->db_accessor("id, japanese", "Mlang", "id = $id");
-                      $item[$j] .= "<p class=\"japanese\">".$get_japanese[0]['japanese']."</p>";
-                      $j++;
-                    }
-                  }
-                }
+
+                // lang-boxの閉じタグを追加
                 for ($j=0; $j < $foodCounter; $j++) {
                   $item[$j] .= "</div>";
                 }
               }
+
+
+              // 料理にセットで付いてくるオプションを表示
+              for ($j=0; $j < $foodCounter; $j++) {
+                if ($key === "food-option-".$j) {
+                  foreach ($value as $foodOptionKey => $foodOption) {
+                    if ($foodOption == "miso-soup") {
+                      $item[$j] .= "<p class=\"miso-soup\">味噌汁付き</p>";
+                    }
+
+                    if ($foodOption == "rice") {
+                      $item[$j] .= "<p class=\"rice\">ご飯付き</p>";
+                    }
+
+                    if ($foodOption == "mini-soba") {
+                      $item[$j] .= "<p class=\"mini-soba\">ミニそば付き</p>";
+                    }
+                  }
+                }
+              }
+
 
               // 金額を表示
               if ($key == "price") {
@@ -81,22 +105,6 @@
                     $item[$j] .= "<p class=\"price\">".$price."YEN</p>";
                   } elseif ($_POST["tax"] == "exclusive") {
                     $item[$j] .= "<p class=\"price\">".$price."YEN +TAX</p>";
-                  }
-                  $j++;
-                }
-              }
-
-
-              // 料理にセットで付いてくるオプションを表示
-              if ($key == "food-option-1") {
-                $j = 0;
-                foreach ($value as $foodOptionKey => $foodOption) {
-                  if ($foodOption == "miso-soup") {
-                    $item[$j] .= "<p class=\"miso-soup\">味噌汁付き</p>";
-                  } elseif ($foodOption == "rice") {
-                    $item[$j] .= "<p class=\"rice\">ご飯付き</p>";
-                  } elseif ($foodOption == "mini-soba") {
-                    $item[$j] .= "<p class=\"mini-soba\">ミニそば付き</p>";
                   }
                   $j++;
                 }
