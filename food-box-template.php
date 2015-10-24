c0d834bcddd1963d1de5a196cd86085fe94825e1
NakanishiTetsuhiro <kaoru6strings@gmail.com>
2015-10-24 14:50:05 +0900

    Added image_upload_form

--- food-box-template.php
+++ food-box-template.php
@@ -1,11 +1,13 @@
 <?php require_once('ConnectDB.php'); ?>
 
+
 <li id="food-box-template" class="item-box">
   <div class="row">
     <div class="col-md-4">
       <div class="form-group">
         <label for="exampleInputEmail1">料理の種類</label>
         <select name="kindBox" class="kind-box form-control">
+
           <?php
           $db = new ConnectDB();
           $get_type = $db->db_accessor("DISTINCT type_id, type", "Mlang", "id >= 1");
@@ -15,16 +17,16 @@
 
           <?php
           $i = 0;
-          foreach ($get_type as $key => $value) {
+          foreach ($get_type as $key => $value) :
           ?>
+
             <option value="<?php echo $value['type_id'] ?>">
-            <?php
-            echo $value['type'];
-            ?>
-          </option>
+              <?php echo $value['type']; ?>
+            </option>
+
           <?php
           $i++;
-          }
+          endforeach;
           ?>
         </select>
       </div>
@@ -33,20 +35,22 @@
       <div class="form-group">
         <label for="exampleInputEmail1">料理名</label>
         <select name="foodNameBox[]" class="food_name_box form-control">
+
           <?php
           $get_foodname = $db->db_accessor("DISTINCT id, japanese", "Mlang", "id >= 1");
           $i = 0;
-          foreach ($get_foodname as $key => $value) {
+          foreach ($get_foodname as $key => $value) :
           ?>
+
           <option value="<?php echo $value['id'] ?>">
-          <?php
-          echo $value['japanese'];
-          ?>
+            <?php echo $value['japanese']; ?>
           </option>
+
           <?php
-            $i++;
-          }
+          $i++;
+          endforeach;
           ?>
+
         </select>
       </div>
     </div>
@@ -64,7 +68,15 @@
     </div>
   </div> <!-- /row -->
   <div class="row">
-    <div class="col-md-5 col-md-offset-7">
+    <div class="col-md-6">
+      <div class="form-group image_upload_form_wrapper">
+        <label for="image_upload_form">画像</label>
+        <input type="file" class="image_upload_form">
+        <p class="help-block">料理の画像を選択してください</p>
+      </div>
+    </div>
+
+    <div class="col-md-6">
       <div class="form-group">
         <label for="price">金額</label>
         <input type="number" class="form-control" name="price[]" id="price" placeholder="メニューの金額を入力してください">
